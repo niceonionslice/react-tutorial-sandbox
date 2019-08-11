db9387cff2ca52675449a9013975bd920bf6236f
niceonionslice <niceonionslice@gmail.com>
2019-08-12 05:17:47 +0900

    可読性を高めるために改行を追加しました。

--- react-tutorial-sandbox/src/Square.js
+++ react-tutorial-sandbox/src/Square.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Square(props) {
   return (
-    <button className='square' onClick={props.onClick}>{props.value}</button>
+    <button
+      className='square'
+      onClick={props.onClick}>{props.value}</button>
   );
 }
 
