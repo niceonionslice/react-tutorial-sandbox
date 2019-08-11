1628b114acd1196ffc534ef318256f71983b52a5
niceonionslice <niceonionslice@gmail.com>
2019-08-12 05:22:07 +0900

    GameコンポーネントにSquare用の状態を保持 Squareで利用する関数を定義 勝者を判定する関数を定義 renderでBoardコンポーネントにsquars,handleclickを渡す処理を追加

--- react-tutorial-sandbox/src/Game.js
+++ react-tutorial-sandbox/src/Game.js
@@ -6,24 +6,62 @@ class Game extends React.Component {
     super(props);
     this.state = {
       xIsNext: true,
+      squares: Array(9).fill(null),
     }
   }
 
-  // 次のプレイヤーを決定する
-  handleNextPlayer() {
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
+      squares: squares,
       xIsNext: !this.state.xIsNext,
     })
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for(let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  getStatus(){
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return status;
   }
 
   render() {
     return (
       <div className='game'>
         <div className='game-board'>
-          <Board nextplaer={this.state.xIsNext} onClick={() => this.handleNextPlayer()}/>
+          <Board squares={this.state.squares} onClick={i => this.handleClick(i)}/>
         </ div>
         <div className='game-info'>
-          <div>{/* status */}</ div>
+          <div>{this.getStatus()}</ div>
           <ol>{/* TODO */}</ ol>
         </ div>
       </ div>
